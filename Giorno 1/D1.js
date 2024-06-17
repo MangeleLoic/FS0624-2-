f8f4f93baa21789ac49dcfea56a13a8879562eb4
sudonona <80674485+sudonona@users.noreply.github.com>
2024-06-17 17:46:28 +0200

    Esercizio Giorno1 Settimana2

--- Giorno 1/D1.js
+++ Giorno 1/D1.js
@@ -21,42 +21,44 @@ REGOLE
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
- let myName = "Loic"
- console.log (myName)  
+let myName = "Loic";
+console.log(myName);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/*let numero1 = 12
-   let numero2 = 20
-   console.log(numero1 + numero2) */
-    
+/*let numero1 = 12;
+   let numero2 = 20;
+   console.log(numero1 + numero2); */
+
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
- let x = 12  
- console.log(x)
+let x = 12;
+console.log(x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-  /*myName = "Mangele"
-  console.log(myName)
+myName = "Mangele";
+console.log(myName);
 
-   const Name = "Loic"
-   console.log(Name) = "Loic" /*
+const Name = "Loic";
+console.log(Name);
+Name = "Hernandez";
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
- let y = 4
-   console.log(y - x)
+let y = 4;
+console.log(y - x);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -64,9 +66,9 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
- let name1 = "john"
-   let name2 = "John"
-   console.log(name1 == name2)
+let name1 = "john";
+let name2 = "John";
+console.log(name1 == name2);
 
-   console.log(name1 === name2)
-    
+console.log(name1 === name2);
+
